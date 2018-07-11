063a87fae62dcc9ac291aa79c135419a8b006b6c
Moses Gitau <gitaumoses4@gmail.com>
2018-07-11 23:58:47 +0300

    Create capitalize function

--- app/src/main/java/com/mg/hotelmanagementsystem/util/Tools.java
+++ app/src/main/java/com/mg/hotelmanagementsystem/util/Tools.java
@@ -1,6 +1,7 @@
 package com.mg.hotelmanagementsystem.util;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 
 import com.mg.hotelmanagementsystem.database.HotelDatabase;
@@ -15,11 +16,18 @@ public class Tools {
     private static User user;
 
     public static void alert(Context context, int titleId, int messageId) {
-        alert(context, context.getString(titleId), context.getString(messageId));
+        if (context != null)
+            alert(context, context.getString(titleId), context.getString(messageId));
     }
 
     public static void alert(Context context, String title, String message) {
-        new AlertDialog.Builder(context).setTitle(title).setMessage(message).create().show();
+        new AlertDialog.Builder(context).setTitle(title).setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create().show();
     }
 
     public static User getCurrentUser(Context context) {
