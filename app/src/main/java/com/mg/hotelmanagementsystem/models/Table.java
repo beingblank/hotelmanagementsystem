3ae51ee9ebe0cb80e690a4174c6e86db0fa9797b
Moses Gitau <gitaumoses4@gmail.com>
2018-07-19 08:47:19 +0300

    Add table model class

---
+++ app/src/main/java/com/mg/hotelmanagementsystem/models/Table.java
@@ -0,0 +1,28 @@
+package com.mg.hotelmanagementsystem.models;
+
+import com.mg.surblime.BaseModel;
+
+/**
+ * Created by moses on 7/12/18.
+ */
+
+public class Table extends HotelBaseModel{
+
+    private String tableName;
+
+    public Table(){
+
+    }
+
+    public Table(String tableName){
+        this.setTableName(tableName);
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+}
