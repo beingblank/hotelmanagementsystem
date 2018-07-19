02f878dde04166e6600b5a7801e6b3426d0f6c94
Moses Gitau <gitaumoses4@gmail.com>
2018-07-19 08:48:17 +0300

    Add fragment to add a table

---
+++ app/src/main/java/com/mg/hotelmanagementsystem/fragments/TablesFragment.java
@@ -0,0 +1,36 @@
+package com.mg.hotelmanagementsystem.fragments;
+
+import android.databinding.ViewDataBinding;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mg.hotelmanagementsystem.BR;
+import com.mg.hotelmanagementsystem.R;
+import com.mg.hotelmanagementsystem.databinding.FragmentTablesBinding;
+import com.mg.hotelmanagementsystem.dialogs.AddTableDialog;
+import com.mg.hotelmanagementsystem.models.Table;
+import com.mg.hotelmanagementsystem.models.viewmodels.TablesViewModel;
+
+/**
+ * Created by moses on 7/12/18.
+ */
+
+public class TablesFragment extends BaseCollectionFragment<TablesViewModel, Table> {
+    public TablesFragment() {
+        super(TablesViewModel.class, Table.class);
+    }
+
+    @Override
+    public void onItemClick(View view, Table table, int index) {
+
+    }
+
+    @Override
+    public String getRoot() {
+        return "tables";
+    }
+}
