98b10bc42e6ba3ce94ce3a926fab92d85b2dba47
Moses Gitau <gitaumoses4@gmail.com>
2018-07-20 08:50:58 +0300

    Add fragment to display meals

--- app/src/main/java/com/mg/hotelmanagementsystem/fragments/MealsFragment.java
+++ app/src/main/java/com/mg/hotelmanagementsystem/fragments/MealsFragment.java
@@ -1,5 +1,6 @@
 package com.mg.hotelmanagementsystem.fragments;
 
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -20,6 +21,7 @@ import com.mg.hotelmanagementsystem.databinding.FragmentMealsBinding;
 import com.mg.hotelmanagementsystem.dialogs.AddMealDialog;
 import com.mg.hotelmanagementsystem.models.Meal;
 import com.mg.hotelmanagementsystem.models.viewmodels.MealsViewModel;
+import com.mg.surblime.ui.ResourceCollectionFragment;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,85 +30,37 @@ import java.util.Map;
  * Created by moses on 7/11/18.
  */
 
-public class MealsFragment extends BaseFragment {
+public class MealsFragment extends BaseCollectionFragment<MealsViewModel, Meal> {
 
-
-    private FragmentMealsBinding fragmentMealsBinding;
-    private MealsViewModel mealsViewModel;
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        fragmentMealsBinding = FragmentMealsBinding.inflate(inflater, container, false);
-
-        return fragmentMealsBinding.getRoot();
+    public MealsFragment() {
+        super(MealsViewModel.class, Meal.class);
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onItemClick(View view, Meal meal, int index) {
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.addMealFAB).setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.addMealFAB).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new AddMealDialog().show(getChildFragmentManager(), "add_meal_dialog");
             }
         });
-        mealsViewModel = new MealsViewModel();
-        fragmentMealsBinding.setMealViewModel(mealsViewModel);
-        syncMeals();
     }
 
-    private void bind() {
-        fragmentMealsBinding.setVariable(BR.mealViewModel, mealsViewModel);
-        fragmentMealsBinding.executePendingBindings();
+    @Override
+    public void loadResource(boolean loadFromCache) {
+        super.loadResource(loadFromCache);
+        setSwipeToRefreshEnabled(false);
     }
 
-    private void syncMeals() {
-        new FirebaseTransaction(getContext())
-                .child("meals").read(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (mealsViewModel.items.isEmpty()) {
-                    for (DataSnapshot child : dataSnapshot.getChildren()) {
-                        mealsViewModel.items.add(child.getValue(Meal.class));
-                    }
-                    bind();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-        new FirebaseTransaction(getContext(), false)
-                .child("meals").readChildren(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                mealsViewModel.items.add(dataSnapshot.getValue(Meal.class));
-                bind();
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+    @Override
+    public String getRoot() {
+        return "meals";
     }
 }
